0170957a67dfdcd4d7b9fd1038f77e5aa5a4e37d
Slayer Ranger <tanc7@unlv.nevada.edu>
2022-05-08 19:36:59 -0700

    Added recursive template parser

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,17 +1,69 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
+
+var functions = template.FuncMap{}
 
 //RenderTemplate renders templates with HTML templates
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
+	tc, err := CreateTemplateCache()
 	if err != nil {
-		fmt.Println("Error parsing template: ", err)
-		return
+		log.Fatal(err)
 	}
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal(err)
+	}
+	buf := new(bytes.Buffer)
+	_ = t.Execute(buf, nil)
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		fmt.Println("Error writing template to browser", err)
+	}
+	//parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	//err := parsedTemplate.Execute(w, nil)
+	//if err != nil {
+	//	fmt.Println("Error parsing template: ", err)
+	//	return
+	//}
+}
+
+//CreateTemplateCache creates a template cache as a map
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+	pages, err := filepath.Glob("./templates/*.page.tmpl.html")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+
+		name := filepath.Base(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl.html")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl.html")
+		}
+
+		if err != nil {
+			return myCache, err
+		}
+		myCache[name] = ts
+	}
+	return myCache, nil
 }
