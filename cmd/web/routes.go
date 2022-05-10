516be26f2ae646699d423bf67ca07e01e666b75d
Slayer Ranger <tanc7@unlv.nevada.edu>
2022-05-10 07:26:05 -0700

    Testing goland git features

---
+++ cmd/web/routes.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/tanc7/go-course/pkg/config"
+	"net/http"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tanc7/go-course
 
 go 1.18
+
+require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
