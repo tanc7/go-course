84dbba5585c17ad3e441f0429bdac5b722654658
Slayer Ranger <tanc7@unlv.nevada.edu>
2022-05-11 17:24:41 -0700

    Demonstrated sessions module by logging IP addresses

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -27,6 +27,9 @@ func NewHandlers(r *Repository) {
 
 // The first arg specifies a "receiver" which is the instance of Repository struct, you need to set it to render.repo.repository in main because it specifies a receiver now.
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
 	render.RenderTemplate(w, "home.page.tmpl.html", &models.TemplateData{})
 }
 
@@ -39,6 +42,10 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	// send data to template
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello, again."
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
 	// send data to template
 	render.RenderTemplate(w, "about.page.tmpl.html", &models.TemplateData{
 		StringMap: stringMap,
